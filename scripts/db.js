27aad474e1b7b14f219a5dff67bc9526a5f5fa1c
TheYan3 <yannic-jundt@gmx.de>
2025-08-28 17:15:11 +0200

    add questions

---
+++ scripts/db.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    "question": "Welches Unternehmen hat die Spielreihe Super Mario entwickelt?",
+    "answer_1": "Sega",
+    "answer_2": "Sony",
+    "answer_3": "Nintendo",
+    "answer_4": "Microsoft",
+    "right_answer": 3
+  },
+  {
+    "question": "In welchem Spiel sammelt man Ringe als Hauptziel?",
+    "answer_1": "Sonic the Hedgehog",
+    "answer_2": "Crash Bandicoot",
+    "answer_3": "Super Mario 64",
+    "answer_4": "Donkey Kong",
+    "right_answer": 1
+  },
+  {
+    "question": "Wie heißt der berühmte Endgegner in vielen Mario-Spielen?",
+    "answer_1": "Ganondorf",
+    "answer_2": "Bowser",
+    "answer_3": "Ridley",
+    "answer_4": "Dr. Eggman",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Spiel ist ein Battle-Royale-Titel?",
+    "answer_1": "Minecraft",
+    "answer_2": "Fortnite",
+    "answer_3": "The Witcher 3",
+    "answer_4": "FIFA 23",
+    "right_answer": 2
+  },
+  {
+    "question": "In welchem Spiel kämpfst du gegen Creeper?",
+    "answer_1": "Terraria",
+    "answer_2": "Minecraft",
+    "answer_3": "Roblox",
+    "answer_4": "Subnautica",
+    "right_answer": 2
+  },
+  {
+    "question": "Wie heißt die Spielkonsole von Microsoft?",
+    "answer_1": "PlayStation",
+    "answer_2": "Xbox",
+    "answer_3": "GameCube",
+    "answer_4": "Switch",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche Farbe hat Pac-Man?",
+    "answer_1": "Rot",
+    "answer_2": "Blau",
+    "answer_3": "Gelb",
+    "answer_4": "Grün",
+    "right_answer": 3
+  },
+  {
+    "question": "In welchem Spiel sagt man oft 'Headshot'?",
+    "answer_1": "Counter-Strike",
+    "answer_2": "Pokémon",
+    "answer_3": "Animal Crossing",
+    "answer_4": "The Sims",
+    "right_answer": 1
+  },
+  {
+    "question": "Wie heißt die Hauptfigur aus The Legend of Zelda?",
+    "answer_1": "Link",
+    "answer_2": "Zelda",
+    "answer_3": "Sheik",
+    "answer_4": "Epona",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches dieser Spiele ist ein Rennspiel?",
+    "answer_1": "Mario Kart",
+    "answer_2": "Skyrim",
+    "answer_3": "Overwatch",
+    "answer_4": "Starcraft",
+    "right_answer": 1
+  }
+];
