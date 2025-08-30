eb758e9985ad22c4b64751d6ed0b4aa1316d43ee
TheYan3 <yannic-jundt@gmx.de>
2025-08-30 13:37:10 +0200

    add function next question

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let questionRef = questions[currentQuestion]
 
 function init() {
      showQuestionAndAnswers() 
+     btndisable()
 }
 
 function showQuestionAndAnswers() {
     document.getElementById("questionLine").innerHTML = questionRef.question;
+    document.getElementById("btnRight").disabled = true;
 
     for (let i = 1; i <= 4; i++) {
         document.getElementById("answerLine" + i).innerHTML = questionRef["answer_" + i];
@@ -16,8 +18,35 @@ function showQuestionAndAnswers() {
 function answer(i) {
     if (i == questionRef.right_answer){
         document.getElementById("answerLine" + i).classList.add("bg-success")
+        document.getElementById("btnRight").disabled = false;
+        setTimeout(() => nextQuestion(i), 2500);
     }else {
          document.getElementById("answerLine" + i).classList.add("bg-danger")
     }
 
+}
+
+function nextQuestion(i) {
+    currentQuestion += 1
+    questionRef = questions[currentQuestion]
+    document.getElementById("answerLine" + i).classList.remove("bg-success")
+    document.getElementById("btnLeft").disabled = false;
+    showQuestionAndAnswers()
+    
+}
+
+function lastQuestion() {
+
+    if (currentQuestion >= 0) {
+         currentQuestion -= 1
+         questionRef = questions[currentQuestion]
+
+         showQuestionAndAnswers()
+    }
+ 
+}
+
+function btndisable() {
+    document.getElementById("btnLeft").disabled = true;
+    
 }
